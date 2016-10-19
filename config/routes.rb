add67389918b771375739d61a98974609ab4f518
rafeeq-123 <rafeeq-123@hotmail.com>
2016-10-19 10:31:01 -0700

    added twilio gem and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'messages/reply'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :messages do
+     collection do
+    post 'reply'
+    end
+  end
 end
